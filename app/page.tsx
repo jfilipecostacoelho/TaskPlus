2ae127371dc060b63fd0db2d223483611f965bf0
jfilipecostacoelho <jfilipecostacoelho@gmail.com>
2025-02-15 18:04:10 +0000

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Task Plus | Organize your tasks in a simple way</h1>
+    </div>
+  );
+}
