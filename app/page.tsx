063fc0745aa4d03c8bf75a0bd6f5c6f1677c68d1
jfilipecostacoelho <jfilipecostacoelho@gmail.com>
2025-02-17 10:55:02 +0000

    First implementation, first styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import heroImg from "../public/assets/hero.png"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Task Plus | Organize your tasks in a simple way!</h1>
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Tasks logo"
+            src={heroImg}
+            priority
+          />
+          <h1 className={styles.title}>
+            Organize your tasks in a <br/>simple way! 
+          </h1>
+        </div>
+        
+      </main>
     </div>
   );
 }
