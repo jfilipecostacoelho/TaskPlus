7f5186a1955b1159b4a0e414a9b770c7ed716bc8
jfilipecostacoelho <jfilipecostacoelho@gmail.com>
2025-02-20 08:16:17 +0000

    Fixed bug in authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
     ],
-    secret: process.env.JWT_SECRET as string, 
+    secret: process.env.JWT_SECRET!, 
 };
 
 const handler = NextAuth(authOptions);
