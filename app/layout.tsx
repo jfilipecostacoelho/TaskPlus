221ca287df7fa5127286fa3a2f22da5b0068ce07
jfilipecostacoelho <jfilipecostacoelho@gmail.com>
2025-02-20 08:23:19 +0000

    Changed SessionProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
+"use client";
 import "./globals.css";
-import {Header} from "../src/components/header/index";
+import { Header } from "../src/components/header/index";
 import SessionProvider from "../src/components/session/SessionProvider";
-import { Session } from "next-auth";
 
 export default function RootLayout({
   children,
-  session
 }: {
   children: React.ReactNode;
-  session: Session;
 }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider session={session}>
-          <Header/>
+        <SessionProvider>
+          <Header />
+          {children}
         </SessionProvider>
-        {children}
       </body>
     </html>
   );
